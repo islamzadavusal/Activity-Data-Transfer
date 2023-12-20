44681361cc0425d4377bb00425561396e9600dc2
Vusal Islamzada <vusal.islamzada.01@gmail.com>
2023-12-20 21:44:31 +0400

    Four

--- app/src/main/java/com/islamzada/project2/features/productlist/MainViewModel.kt
+++ app/src/main/java/com/islamzada/project2/features/productlist/MainViewModel.kt
@@ -7,7 +7,14 @@ class MainViewModel: ViewModel() {
 
     var addProductObserver = MutableLiveData<Boolean>()
 
+    var result = MutableLiveData<String>()
+
     fun onAddNewProduct(){
         addProductObserver.postValue(true)
+    }
+
+    fun newProductAdded(){
+        result.postValue("")
+
     }
 }
